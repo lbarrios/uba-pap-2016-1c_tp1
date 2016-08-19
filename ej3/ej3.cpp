ca88657141d8cba0621bc41ca8e4ead0a353593b
Ezequielk <ezequielginsberg@gmail.com>
2016-08-19 17:31:34 -0300

    ej 3 creo que esta bien, del 4 tengo dudas sobre la complejidad

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -1,7 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-  /* code */
+int main(int argc, char const *argv[]) {
+  int N;
+  cin >> N;
+  int arreglo[N];
+  int sumas[N];
+  for(int i = 0; i < N; i++) {
+    cin >> arreglo[i];
+    sumas[i] = 0;
+  }
+  sumas[0] = max(sumas[0], arreglo[0]);
+  int res = 0;
+  for(int i = 1; i < N; i++) {
+    sumas[i] = max(sumas[i-1] + arreglo[i], arreglo[i]);
+    if(sumas[i] > res) {
+      res = sumas[i];
+    }
+  }
+  cout << res << endl;
   return 0;
 }
--- ej4/ej4.cpp
+++ ej4/ej4.cpp
@@ -1,9 +1,80 @@
+#include <iostream>
 #include "../catedra/libs/Matriz.h"
 
 using namespace std;
 
-int main(int argc, char const *argv[])
-{
-  /* code */
+bool aux(Matriz M, Matriz arreglo[], int N, int L, int inicio, int fin) {
+  if(fin-inicio <= L) {
+    //ad hoc
+    //multiplicar las L matrices, agregando por izquierda o por derecha sea cual fuese el caso
+    int diferencia = L-(fin-inicio); //cuantas posiciones me faltan para llegar a L
+    while(inicio >= 0 && diferencia > 0) {
+      inicio--;
+      diferencia--;
+    }
+    if(diferencia > 0) {
+      while(fin <= N && diferencia > 0) {
+        fin++;
+        diferencia--;
+      }
+    }
+    if(diferencia > 0) return false;
+    //se llego a L con inicio y fin (hay q comprobar que fin-inicio = L
+    Matriz producto = id();
+    for(int i = inicio; i < fin; i++) {
+      producto = producto * arreglo[i];
+    }
+    return (producto == M);
+  } else { //dividir y combinar
+    int medio = (inicio+fin)/2;
+    bool izq = aux(M, arreglo, N, L, inicio, medio);
+    bool der = aux(M, arreglo, N, L, medio, fin);
+    if(izq || der) { // si alguno dio true, significa que existe el subarreglo
+      return true;
+    } else { //sino, falta todavía ver el medio
+      inicio = medio - L;
+      if(inicio < 0) inicio = 0;
+      fin = medio + L;
+      if(fin > N) fin = N;
+      bool res = false;
+      for(int i = inicio; i < fin+1-L; i++) { // veo todas las opciones que pasen por el medio, el tema es que no se si esto es N^2 ....
+        Matriz producto = id();
+        for(int j = i; j < i+L; j++) {
+          producto = producto*arreglo[j];
+        }
+        if(producto == M) {
+          res = true;
+        }
+      }
+      return res;
+    }
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  int N;
+  cin >> N;
+  int L;
+  cin >> L;
+  vector<int> vectorM(9);
+  Matriz arreglo[N];
+  for(int i = 0; i < 9; i++) {
+    cin >> vectorM[i];
+  }
+  Matriz M = Matriz(vectorM);
+  for(int i = 0; i < N; i++) {
+    vector<int> matrizAux(9);
+    for(int j = 0; j < 9; j++) {
+      cin >> matrizAux[j];
+    }
+    arreglo[i] = Matriz(matrizAux);
+  }
+  
+  if(aux(M, arreglo, N, L, 0, N)) {
+    cout << "SI" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+  
   return 0;
 }
