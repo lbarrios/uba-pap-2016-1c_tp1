772c5b987858f978e1ed985683843be6de03b7c2
Ezequielk <ezequielginsberg@gmail.com>
2016-08-28 23:55:43 -0300

    pequeña modificación al ej3
    ayuda a la explicación en el informe

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -10,7 +10,7 @@ int main() {
 
   // Creo dos arreglos de int:
   // "A" que va a guardar los A_i que vienen del input
-  // "sumaMax" que va a guardar en la posicion "i" la suma del subarreglo de suma maxima considerando A[0, i]
+  // "sumaMax" que va a guardar en la posicion "i" la suma del subarreglo de suma maxima considerando A[0, i] con la condicion de que la posicion "i" pertenezca al subarreglo
   int A[N];
   int sumaMax[N];
 
@@ -19,13 +19,12 @@ int main() {
     cin >> A[i];
   }
 
-  // La primer posicion va a ser el maximo entre el subarreglo vacio (cuya suma es igual a 0), o el primer 
-  // valor de A
-  sumaMax[0] = max(0, A[0]);
+  // La primer posicion de sumaMax va a ser el primer valor de A
+  sumaMax[0] = A[0];
 
   // Calculamos linealmente todas las posiciones de sumaMax usando la funcion de recurrencia a la vez que 
   // vamos guardando un maximo local para que al finalizar el recorrido lineal tengamos el maximo absoluto 
-  // y asi poder devolverlo
+  // y asi poder devolver el maximo entre ese maximo absoluto y 0 (el subarreglo vacio)
   int res = sumaMax[0];
   for(int i = 1; i < N; i++) {
     // Usamos la formulacion recursiva que enunciamos en el informe para calcular la posicion i de sumaMax
@@ -35,7 +34,8 @@ int main() {
     }
   }
 
-  // res guarda el maximo absoluto, que hay que devolver
+  // res guarda el maximo absoluto, que hay que devolver. Pero antes hay que compararlo con 0 ya que puede convenir considerar el subarreglo vacio
+  res = (res > 0)? res : 0;
   cout << res << endl;
 
   return 0;
